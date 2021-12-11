52e80776fccf72e7d23d4be8eab42e3c83f3fa0a
caroline parra gomes <Cargomes@server01.foursys.local>
2021-12-11 17:07:54 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] arg) {
+
+        //Crie um programa para que apresente seu salário e
+        //vale-refeição;
+
+        double salario;
+        double valeRefeicao;
+
+        salario = 1350.00;
+        valeRefeicao = 410.00;
+
+        System.out.println(salario);
+        System.out.println(valeRefeicao);
+    }
+}
